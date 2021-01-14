ba5ef5c03e7397c02bfa2c1ed1487243867ed4b5
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-01-14 11:53:04 +0530

    navbar add logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import './styles.css';
 
 import HomeComponent from './components/HomeComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-floral-white">
       <HomeComponent/>
     </div>
   );
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Logo from '../logo.svg';
 
 import '../styles.css';
 
@@ -6,7 +7,14 @@ export default class NavigationBar extends Component {
     render() {
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-purple">
+                <div className="container-fluid">
+                    <div className="navbar-brand p-0" href="#">
+                        <img src={Logo} alt="logo" width="40" height="40"/>
+                    </div>   
 
+                    <button type="button" class="btn btn-primary btn-sm">Show Ads</button>
+
+                </div>
             </nav>
         );
     }
