612c9ecba0a3100dfbd7eae0840ff8901c8bba06
Thisarahetz <thisarayashodagcc@gmail.com>
2021-01-15 13:34:24 +0530

    Add login button and interface

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 
 import NavigationBar from './NavigationBar';
 import AdHome from './ad/AdHome';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Register from './User/Register';
 
 
 export default class HomeComponent extends Component {
     render() {
         return(
             <div>
-                <NavigationBar/>
+                <BrowserRouter>
                 
+                <NavigationBar/>
+                <Switch>
+              <Route path="/register"><Register></Register></Route>  
+                </Switch>
                 <AdHome/>
-
+                </BrowserRouter>
             </div>
         );
     }
