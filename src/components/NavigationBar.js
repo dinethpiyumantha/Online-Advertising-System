faebddd35b19601f90095879c48703e8195e23ca
Thisarahetz <thisarayashodagcc@gmail.com>
2021-01-15 22:51:05 +0530

    update login button and interface

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import { FaUserAlt } from 'react-icons/fa';
 import { FaUserSlash } from 'react-icons/fa';
 import { MdSettings } from 'react-icons/md';
+import Login from './User/Login';
 
 export default class NavigationBar extends Component {
     render() {
@@ -32,46 +33,10 @@ export default class NavigationBar extends Component {
                         </div>
                         </ul>   
                     </div>
-                    <button class="btn btn-success m-2" data-toggle="modal" data-target="#loginPage">Login</button>
-                    <button class="btn btn-outline-warning " data-toggle="modal" data-target="#RegisterPage">Register</button>
+                    {/* <button class="btn btn-success m-2" data-toggle="modal" data-target="#loginPage">Login</button> */}
+                    <Login></Login>
+                    <button class="btn btn-warning "><Link to="/register">Register</Link> </button>
                 </div>
-
-                {/* Login and Register model poop css use */}
-
-                <div className="modal fade" tabindex="-1" id="loginPage" role="dialog">
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Login</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                                <form>
-                                <div className="form-group">
-                                    <label>Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                                </div>
-                                <div className="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-                                </div>
-                                <div className="form-group form-check">
-                                    <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
-                                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                                </div>
-                                <button type="submit" className="btn btn-primary">Login</button>
-                                </form>
-                    </div>
-                    <div class="modal-footer">
-                        <p> Please Register first <button className="badge-info"><Link className="nav-link" to="/register">Register</Link> </button>  </p>
-                    </div>
-                    </div>
-                </div>
-                </div>
-
             </nav>
         );
     }
